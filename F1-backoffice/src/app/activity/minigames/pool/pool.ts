2b37270742d8f9a339bb684ca8f2fd6f93b2b6c2
Shomaa Azelkara <sshomaa@gmail.com>
2022-11-30 01:14:18 +0100

    add pool mini game

--- F1-backoffice/src/app/activity/minigames/pool/pool.ts
+++ F1-backoffice/src/app/activity/minigames/pool/pool.ts
@@ -1,5 +1,29 @@
+import { Mesh } from "babylonjs/index";
 import { MiniGameState } from "../IMiniGame";
 import{IMiniGame} from "../IMiniGame";
+
+
+
+export enum PoolGameState {
+    CHANGE_DIRECTION,
+    ADJUST_POWER,
+    WAITING_FOR_BALL,
+    CHANGE_PLAYER,
+};
+
+
+
+
+export class PoolTable {
+
+    killBox : Mesh[];
+
+    constructor() {
+        this.killBox = [];
+    }
+}
+
+
 
 export class Pool implements IMiniGame {
 
@@ -24,4 +48,48 @@ export class Pool implements IMiniGame {
     GetState(): MiniGameState { 
         return this.stateMiniGame; 
     }
+
+
+    PrepareChangeDirection(): void {
+        // load orbit camera
+
+        // set target camera to white ball
+
+        // set camera position to white ball
+
+        throw new Error("Method PrepareChangeDirection not implemented.");
+    }   
+
+    PrepareAdjustPower(): void {
+
+        // set camera position to white ball
+
+        // set target camera to white ball
+
+        // display UI
+
+        // display power bar
+
+        // display pool cue
+        throw new Error("Method PrepareAdjustPower not implemented.");
+    }
+
+    PrepareWaitingForBall(): void {
+
+        // set tatget camera to pool table
+
+        // set camera position to pool table
+
+        // hide UI
+
+        // hide power bar
+
+        throw new Error("Method PrepareWaitingForBall not implemented.");
+    }
+
+    PrepareChangePlayer(): void {
+        throw new Error("Method PrepareChangePlayer not implemented.");
+    }
+
+
 }
