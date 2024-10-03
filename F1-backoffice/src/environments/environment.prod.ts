3fe7eb9ab8bd168f2c83427d8817b50e3151659c
Shomaa Azelkara <sshomaa@gmail.com>
2024-10-03 21:25:41 +0200

    change prod env

--- F1-backoffice/src/environments/environment.prod.ts
+++ F1-backoffice/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   AZURE_STORAGE_ACCOUNT_NAME: 'f1backoffice',
-  API_ADRESS: 'https://localhost:7279/api'
+  API_ADRESS: 'https://localhost:7279/api',
+  BLOB_STORAGE: 'https://f1backoffice.blob.core.windows.net',
+  AZURE_STORAGE_ACCOUNT_KEY: 'Default',
 };
