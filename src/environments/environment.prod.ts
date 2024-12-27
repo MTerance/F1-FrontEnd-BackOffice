6ecf57a62e6aef2a2c8440d336a8ef33fc0fb2b3
Shomaa Azelkara <sshomaa@gmail.com>
2024-12-27 19:24:27 +0100

    remise des fichiers env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
   production: true,
+  AZURE_STORAGE_ACCOUNT_NAME: 'gamestorageaccountdb',
+  API_ADRESS: 'https://localhost:7279/api',
+  BLOB_STORAGE: 'https://f1backoffice.blob.core.windows.net',
+  AZURE_STORAGE_ACCOUNT_KEY: 'Default',
+  API_KEY:'API_KEY',
+  AZURE_STORAGE_CLIENT_ID: "CLIENT_ID",
+  CONNECTION_STRING: "CONNECTION_STRING",
+  TOKEN_SAS:"TOKEN_SAS",
+  URL_BLOB:"URL_BLOB",
+  URL_FILE:"URL_FILE",
+  AZURE_CLIENT_ID: "AZURE_CLIENT_ID",
+  AZURE_TENANT_ID: "AZURE_TENANT_ID",
+  AZURE_CLIENT_SECRET: "AZURE_CLIENT_SECRET",
+  KEYVAULT_URL: "KEYVAULT_URL",
 };
